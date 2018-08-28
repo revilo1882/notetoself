3b75e795e70b4eefd5ea8b2d289cfe48637d60d2
Revilo1882 <oliverrharris@gmail.com>
2018-08-28 18:18:13 +0100

    Add react bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h2>Note to Self</h2>
+        <Form>
+          <FormControl />
+          <Button>Submit</Button>
+        </Form>
       </div>
     )
   }
